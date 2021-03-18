00a34e2af71bb68136325c412bab16b8327e59a5
AUnicornWithNoLife <benjamin.barratt@icloud.com>
2021-03-18 20:33:03 +0000

    Update del.php

--- src/code/api/del.php
+++ src/code/api/del.php
@@ -11,4 +11,19 @@ if (! in_array($file, $films['films']))
     die("not in array");
 }
 
+$art = $films['art'][$file];
+$film = $films['file'][$file];
+
+unlink("../../data/art/$art");
+unlink("../../data/films/$film");
+
+unset($films['films'][$file]);
+unset($films['art'][$file]);
+unset($films['file'][$file]);
+unset($films['progress'][$file]);
+
+$myfile = fopen("../../data/saved/info.json", "w") or die("Unable to open file!");
+fwrite($myfile, $fj);
+fclose($myfile);
+
 ?>
