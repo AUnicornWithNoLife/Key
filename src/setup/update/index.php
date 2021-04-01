8c94cbacdec5850a3b76844ae739e67cc191dd3b
AUnicornWithNoLife <benjamin.barratt@icloud.com>
2021-04-01 20:37:59 +0100

    Update index.php

--- src/setup/update/index.php
+++ src/setup/update/index.php
@@ -12,8 +12,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <title>Update</title>
+
+    <link rel="stylesheet" href="../../code/css/main.css">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com"> 
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
+
+    <link rel="shortcut icon" href="../../favicon.ico" type="image/x-icon">
+    <link rel="icon" href="../../favicon.ico" type="image/x-icon">
 </head>
 <body>
-    
+    <h1>Updating your system</h1>
 </body>
 </html>
